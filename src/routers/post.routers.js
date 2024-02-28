36990f249c2f7f26ed49541539ea183d155f759b
Susanta Mandal <susantamandal17@gmail.com>
2024-02-29 01:47:27 +0530

    new routes added

--- src/routers/post.routers.js
+++ src/routers/post.routers.js
@@ -1,13 +1,19 @@
 import express from 'express';
 
-import { getPosts, createPost, updatePost, deletePost } from '../controllers/post.controllers.js'
+import { verifyAccessToken } from '../middlewares/apigateway.middlewares.js';
+import { getPosts, createPost, updatePost, deletePost, getComments, createComment, updateComment, deleteComment} from '../controllers/post.controllers.js'
+import { upload } from "../middlewares/multer.middlewares.js"
 
 const router = express.Router();
 
-router.get('/posts', getPosts);
-router.get('/post/:id', getPosts);
-router.post('/post', createPost );
-router.put('/post/:id', updatePost );
-router.delete('/post/:id', deletePost );
+router.get('/:uid/posts', verifyAccessToken, getPosts);
+router.post('/post',  verifyAccessToken, upload.array('media', 10), createPost );
+router.patch('/post', verifyAccessToken, upload.array('media', 10), updatePost );
+router.delete('/post', verifyAccessToken, deletePost );
+
+router.get('/:pid/comments', verifyAccessToken, getComments);
+router.post('/comment',  verifyAccessToken, upload.single('media'), createComment );
+router.patch('/comment',  verifyAccessToken, upload.single('media'), updateComment );
+router.delete('/comment', verifyAccessToken, deleteComment );
 
 export default router;
--- src/routers/user.routers.js
+++ src/routers/user.routers.js
@@ -1,17 +1,19 @@
-import express from 'express';
+import express from "express";
 
-import {createUser, updateUser, deleteUser,
-    getFriends, getActionsOnFriend, friendRequest, getLoginUser, getUser} from '../controllers/user.controllers.js';
+import { getLoginUser, getUser, updateProfileAvatar } from "../controllers/user.controllers.js";
 import { verifyAccessToken } from '../middlewares/apigateway.middlewares.js';
+import { upload } from "../middlewares/multer.middlewares.js"
 
 const router = express.Router();
-router.post('/', createUser);
+// router.post('/', createUser);
 router.get('/loginuser', verifyAccessToken, getLoginUser);
 router.get('/user', verifyAccessToken, getUser);
-router.get('/:id/friends', getFriends);
-router.get('/:uid/friend/:fid/actions', getActionsOnFriend);
-router.post('/:id/friendrequest', friendRequest);
-router.patch('/:id', updateUser);
-router.delete('/:id', deleteUser);
+router.patch('/avatar', verifyAccessToken, upload.single('avatar'), updateProfileAvatar);
+router.delete('/avatar', verifyAccessToken, updateProfileAvatar);
+// router.get('/:id/friends', getFriends);
+// router.get('/:uid/friend/:fid/actions', getActionsOnFriend);
+// router.post('/:id/friendrequest', friendRequest);
+// router.patch('/:id', updateUser);
+// router.delete('/:id', deleteUser);
 
 export default router;
