3c0fbaf0b820b81bd277cbb068488776e2b87763
Susanta Mandal <susantamandal17@gmail.com>
2024-02-29 01:52:48 +0530

    route methods added and modified

--- src/controllers/post.controllers.js
+++ src/controllers/post.controllers.js
@@ -2,63 +2,373 @@ import mongoose from "mongoose"
 
 import logger from '../logger/index.js';
 import UserModel from "../models/user.models.js";
-import FriendModel from "../models/friend.models.js";
 import PostModel from "../models/post.models.js";
-import { URL_POST } from '../utils/constants.utils.js'
+import { API_REQUEST, URL_POST, GET_POST_DEFAULT_OFFSET, GET_POST_DEFAULT_LIMIT, GET_COMMENT_DEFAULT_LIMIT, GET_COMMENT_DEFAULT_OFFSET } from '../utils/constants.utils.js'
+import { destroyFromCloudinary, uploadOnCloudinary } from "../utils/cloudinary.utils.js";
+import { setUniquePostMediaFileNames, setUniqueCommentMediaFileName } from "../utils/setUniqueFileNames.utils.js";
+import { ApiError } from "../utils/apiError.utils.js";
+import { ApiResponse } from "../utils/apiResponse.utils.js";
+import CommentModel from "../models/comment.models.js";
 
 export const getPosts = async (req, res) => {
 
-    logger.info(`getPosts starts!`)
+    logger.info(`${API_REQUEST} ${URL_POST}`);
+    logger.info(`getPost starts!`)
+    try {
 
-    const creatorId = req.query.creatorId
-    const postedTo = req.query.postedTo
-    let limit = req.query.limit
-    let offset = req.query.offset
+        let { uid: _id } = req.params, { offset, limit } = req.query;
 
-    logger.info(creatorId, postedTo)
-    let filters = []
-    if(creatorId) filters.push({creatorId})
-    if(postedTo) filters.push({postedTo})
-    if(!offset) offset = 0
-    if(!limit) limit = 5
+        offset = isNaN(offset) ? GET_POST_DEFAULT_OFFSET : parseInt(offset);
+        limit = isNaN(limit) ? GET_POST_DEFAULT_LIMIT : parseInt(limit);
 
-    try{
-        const posts = await PostModel.find(filters.length > 0 ? {$or: filters}: {}).sort({updatedAt:-1}).skip(offset).limit(limit).exec()
-        const count = await PostModel.find(filters.length > 0 ? {$or: filters}: {}).count();
-        logger.info(`getPosts successful!`)
-        let body = {
-            data: posts,
-            total: count,
-            offset: offset,
-            limit: limit
+        if (!_id)
+            throw new ApiError(400, "user id/_id is required to fetch posts");
+
+        let user = (_id === req.user._id ? req.user : await UserModel.findById(_id));
+
+        if (!user) {
+            throw new ApiError(400, `user not found with id ${_id}`)
         }
-        res.status(200).json(body)
+
+        let postAggregate = await PostModel.aggregate([
+            {
+                $match: {
+                    $or: [
+                        { postedOn: user._id },
+                        { createdBy: user._id }
+                    ]
+                }
+            },
+            {
+                $facet: {
+                    posts: [
+
+                        { $sort: { createdAt: -1 } },
+                        { $skip: offset },
+                        { $limit: limit },
+                        {
+                            $lookup: {
+                                from: "metaserver_comments",
+                                localField: "_id",
+                                foreignField: "belongsTo",
+                                as: "comments",
+                                pipeline: [
+                                    {
+                                        $match: {
+                                            parent: mongoose.mongo.ObjectId("000000000000000000000000")
+                                        }
+                                    },
+                                    { $sort: { createdAt: 1 } },
+                                    { $limit: GET_COMMENT_DEFAULT_LIMIT }
+                                ],
+                            }
+                        }
+                    ],
+                    pagination: [
+                        { $count: "total" },
+                        {
+                            $addFields: {
+                                offset: offset,
+                                limit: {
+                                    $cond: [{ $lt: ["$total", limit] }, "$total", limit]
+                                }
+                            }
+                        }
+                    ]
+                }
+            },
+            {
+                $addFields: {
+                    pagination: { $arrayElemAt: ["$pagination", 0] }
+                }
+            }
+        ]);
+
+
+        res.status(200).json(new ApiResponse(200, postAggregate[0]));
     }
-    catch(error){
-        res.status(404).json({ message: error.message })
+    catch (error) {
+
+        logger.info(error.stack)
+        error = new ApiError(error?.statusCode || 400, error.message);
+        res.status(error.statusCode).json(error);
     }
     logger.info(`getPosts ends!`)
+
 }
 
 export const createPost = async (req, res) => {
+    logger.info(`${API_REQUEST} ${URL_POST}`);
     logger.info(`createPost starts!`)
-    try{
-        const newPost = new PostModel(req.body)
-        logger.info(`createPost payload!: ${req.body}`);
-        await newPost.save();
-        logger.info(`createPost successful! ${newPost._id}`);
-        res.status(201).json(newPost);
+    try {
+
+        const { media, postedOn, ...rest } = req.body
+        let post = await PostModel.create({ createdBy: req.user._id, postedOn, ...rest });
+        req.post = post;
+        const mediaLocalFilePaths = await setUniquePostMediaFileNames(req);
+        let mediaCloudinaryUrls = [];
+        for (let mediaLocalPath of mediaLocalFilePaths) {
+            let mediaFromCloud = await uploadOnCloudinary(mediaLocalPath);
+            mediaCloudinaryUrls.push(mediaFromCloud.secure_url);
+        }
+        post = await PostModel.findByIdAndUpdate(post._id, { media: mediaCloudinaryUrls }, { new: true });
+        res.status(201).json(new ApiResponse(201, post));
     }
-    catch(error){
-        res.status(404).json({ message: error.message })
+    catch (error) {
+
+        logger.info(error.stack)
+        error = new ApiError(error?.statusCode || 400, error.message);
+        res.status(error.statusCode).json(error);
     }
     logger.info(`createPost ends!`)
 }
 
 export const deletePost = async (req, res) => {
-    createPost(req, res)
+    logger.info(`${API_REQUEST} ${URL_POST}`);
+    logger.info(`deletePost starts!`)
+    try {
+        let { id, _id } = req.query;
+        _id = _id ? _id : id;
+        if (!_id) {
+            throw new ApiError(400, "post id/_id is required to delete");
+        }
+        const post = await PostModel.findOne({
+            $and: [
+                { _id },
+                {
+                    $or: [
+                        { createdBy: req.user._id },
+                        { postedOn: req.user._id }
+                    ]
+                }
+            ]
+        });
+        if (!post)
+            throw new ApiError(400, `no post found with id ${_id}`);
+        await post.deleteOne();
+        res.status(200).json(new ApiResponse(200, {}, "post deleted successfully"));
+    }
+    catch (error) {
+        logger.info(error.stack)
+        error = new ApiError(error?.statusCode || 400, error.message);
+        res.status(error.statusCode).json(error);
+    }
+    logger.info(`deletePost ends!`)
 }
 
 export const updatePost = async (req, res) => {
-    createPost(req, res)
+    logger.info(`${API_REQUEST} ${URL_POST}`);
+    logger.info(`updatePost starts!`)
+    try {
+
+        let { _id, id, media, ...rest } = req.body
+        _id = _id ? _id : id;
+        if (!_id) {
+            throw new ApiError(400, "post id/_id is required to update");
+        }
+
+        let post = await PostModel.findByIdAndUpdate(_id, { ...rest }, { new: true });
+        req.post = post;
+
+        const mediaLocalFilePaths = await setUniquePostMediaFileNames(req);
+
+        // delete existing media from cloudinary
+        if (post.media.length > 0 && (mediaLocalFilePaths.length > 0 || media === "")) {
+            for (let index = 0; index < post.media.length; index++) {
+                await destroyFromCloudinary(`post-${post._id}-media-${index + 1}`);
+            }
+        }
+
+        let mediaCloudinaryUrls = [];
+
+        for (let mediaLocalPath of mediaLocalFilePaths) {
+            let mediaFromCloud = await uploadOnCloudinary(mediaLocalPath);
+            mediaCloudinaryUrls.push(mediaFromCloud.secure_url);
+        }
+
+        const updatePayload = (mediaCloudinaryUrls.length > 0 ? { media: mediaCloudinaryUrls } : (media === "" ? { media: [] } : {}));
+        post = await PostModel.findByIdAndUpdate(_id, updatePayload, { new: true });
+        res.status(200).json(new ApiResponse(200, post));
+    }
+    catch (error) {
+
+        logger.info(error.stack)
+        error = new ApiError(error?.statusCode || 400, error.message);
+        res.status(error.statusCode).json(error);
+    }
+    logger.info(`updatePost ends!`)
+}
+
+export const getComments = async (req, res) => {
+
+    logger.info(`${API_REQUEST} ${URL_POST}`);
+    logger.info(`getComments starts!`)
+    try {
+
+        let { pid: _id } = req.params, { offset, limit, parent } = req.query;
+
+        offset = isNaN(offset) ? GET_COMMENT_DEFAULT_OFFSET : parseInt(offset);
+        limit = isNaN(limit) ? GET_COMMENT_DEFAULT_LIMIT : parseInt(limit);
+
+        parent = parent || "000000000000000000000000";
+
+        if (!_id)
+            throw new ApiError(400, "post id/_id is required to fetch comments");
+
+        let post = await PostModel.findById(_id);
+
+        if (!post) {
+            throw new ApiError(400, `no post found with id ${_id}`)
+        }
+
+        let comment = await CommentModel.findById(parent);
+
+        if (parent !== "000000000000000000000000" && !comment) {
+            throw new ApiError(400, `no parent comment found with id ${parent}`)
+        }
+
+
+        let commentAggregate = await CommentModel.aggregate([
+            {
+                $match: {
+                    $and: [
+                        { belongsTo: post._id },
+                        { parent: mongoose.mongo.ObjectId(parent) }
+                    ]
+                }
+            },
+            {
+                $facet: {
+                    comments: [
+                        { $sort: { createdAt: 1 } },
+                        { $skip: offset },
+                        { $limit: limit }
+                    ],
+                    pagination: [
+                        { $count: "total" },
+                        {
+                            $addFields: {
+                                offset: offset,
+                                limit: {
+                                    $cond: [{ $lt: ["$total", limit] }, "$total", limit]
+                                }
+                            }
+                        }
+                    ]
+                }
+            },
+            {
+                $addFields: {
+                    pagination: { $arrayElemAt: ["$pagination", 0] }
+                }
+            }
+        ]);
+
+        res.status(200).json(new ApiResponse(200, commentAggregate[0]));
+    }
+    catch (error) {
+
+        logger.info(error.stack)
+        error = new ApiError(error?.statusCode || 400, error.message);
+        res.status(error.statusCode).json(error);
+    }
+    logger.info(`getComments ends!`)
+
+}
+
+export const createComment = async (req, res) => {
+    logger.info(`${API_REQUEST} ${URL_POST}`);
+    logger.info(`createComment starts!`)
+    try {
+
+        let { media, parent, ...rest } = req.body
+
+        if (!parent) {
+            parent = mongoose.Types.ObjectId.createFromHexString("000000000000000000000000");
+        }
+        let comment = await CommentModel.create({ createdBy: req.user._id, parent, ...rest });
+        req.comment = comment;
+
+        const mediaLocalFilePath = await setUniqueCommentMediaFileName(req);
+
+        if (mediaLocalFilePath) {
+            const mediaFromCloud = await uploadOnCloudinary(mediaLocalFilePath);
+            comment = await CommentModel.findByIdAndUpdate(comment._id, { media: mediaFromCloud.secure_url }, { new: true });
+        }
+
+        res.status(201).json(new ApiResponse(201, comment));
+    }
+    catch (error) {
+
+        logger.info(error.stack)
+        error = new ApiError(error?.statusCode || 400, error.message);
+        res.status(error.statusCode).json(error);
+    }
+    logger.info(`createComment ends!`)
+}
+
+export const updateComment = async (req, res) => {
+    logger.info(`${API_REQUEST} ${URL_POST}`);
+    logger.info(`updateComment starts!`)
+    try {
+
+        let { _id, id, media, ...rest } = req.body
+        if (!_id && !id) {
+            throw new ApiError(400, "comment id/_id is required to update");
+        }
+        _id = _id ? _id : id;
+
+        let comment = await CommentModel.findByIdAndUpdate(_id, { ...rest }, { new: true });
+        req.comment = comment;
+
+        const mediaLocalFilePath = await setUniqueCommentMediaFileName(req);
+
+        // delete existing media from cloudinary
+        if (comment.media && (mediaLocalFilePath || media === "")) {
+            await destroyFromCloudinary(`comment-media-${comment._id}`);
+        }
+
+        let mediaFromCloud = {};
+
+        if (mediaLocalFilePath) {
+            mediaFromCloud = await uploadOnCloudinary(mediaLocalFilePath);
+        }
+
+        const updatePayload = (mediaFromCloud.secure_url ? { media: mediaFromCloud.secure_url } : (media === "" ? { media: "" } : {}));
+
+        comment = await CommentModel.findByIdAndUpdate(_id, updatePayload, { new: true });
+
+        res.status(201).json(new ApiResponse(200, comment));
+    }
+    catch (error) {
+
+        logger.info(error.stack)
+        error = new ApiError(error?.statusCode || 400, error.message);
+        res.status(error.statusCode).json(error);
+    }
+    logger.info(`updateComment ends!`)
+}
+
+export const deleteComment = async (req, res) => {
+    logger.info(`${API_REQUEST} ${URL_POST}`);
+    logger.info(`deleteComment starts!`)
+    try {
+        let { id, _id } = req.query;
+        _id = _id ? _id : id;
+        if (!_id) throw new ApiError(400, "comment id/_id is required to delete");
+        const comment = await CommentModel.findById(_id);
+        if (!comment) throw new ApiError(400, `no comment found with id ${_id}`);
+        const post = await PostModel.findById(comment.belongsTo);
+        if (!post) throw new ApiError(400, `no post found for the comment with id ${_id}`);
+        if (!comment.createdBy.equals(req.user._id) && !post.createdBy.equals(req.user._id))
+            throw new ApiError(400, `user cannot delete this comment`);
+        await comment.deleteOne();
+        res.status(200).json(new ApiResponse(200, {}, "comment deleted successfully"));
+    }
+    catch (error) {
+        logger.info(error.stack)
+        error = new ApiError(error?.statusCode || 400, error.message);
+        res.status(error.statusCode).json(error);
+    }
+    logger.info(`deleteComment ends!`)
 }
--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,16 +1,14 @@
-import mongoose from "mongoose";
-
 import logger from '../logger/index.js';
 import UserModel from "../models/user.models.js";
 import FriendModel from "../models/friend.models.js";
-import { asyncHandler } from "../utils/asyncHandler.utils.js";
-
 import {
     API_REQUEST, URL_USER, ACT_MESSAGE, ACT_UNFRIEND, ACT_BLOCK,
     ACT_DELETE_REQUEST, ACT_ADD_FRIEND, ACT_ACCEPT_REQUEST
 } from '../utils/constants.utils.js'
 import { ApiError } from "../utils/apiError.utils.js";
 import { ApiResponse } from "../utils/apiResponse.utils.js";
+import { destroyFromCloudinary, uploadOnCloudinary } from "../utils/cloudinary.utils.js";
+import { setUniqueProfileAvatarFileName } from "../utils/setUniqueFileNames.utils.js";
 
 
 
@@ -68,12 +66,12 @@ export const getUser = async (req, res) => {
 
     try {
 
-        const {id} = req.query;
+        const { id } = req.query;
 
         if (!id)
             throw new ApiError(400, "id is required to fetch user details");
 
-        const user = await UserModel.findById(id).select("_id firstName lastName gender email accountDiabled active lastActiveAt profileImageUrl");
+        const user = await UserModel.findById(id).select("_id firstName lastName gender email accountDiabled active lastActiveAt profileAvatar");
 
         if (!user)
             throw new ApiError(400, `no user found with id ${_id}`);
@@ -89,127 +87,175 @@ export const getUser = async (req, res) => {
     logger.info('getUser ends')
 };
 
-export const createUser = async (req, res) => {
+export const updateProfileAvatar = async (req, res) => {
+
     logger.info(`${API_REQUEST} ${URL_USER}`);
-    logger.info(`createUser starts`);
-    const newUser = new UserModel(req.body);
-    try {
-        logger.info(`createUser payload!: ${req.body}`);
-        await newUser.save();
-        logger.info(`createUser successful! ${newUser._id}`);
-        res.status(201).json(newUser);
-    } catch (error) {
-        logger.error(error.message)
-        res.status(409).json({ message: error.message });
-    }
-    logger.info('createUser ends')
-}
-
-export const updateUser = async (req, res) => {
-    const { id: _id } = req.params;
-    const user = req.body;
-    logger.info(`${API_REQUEST} ${URL_USER}/${_id}`);
-    logger.info(`updateUser starts`);
-    if (!mongoose.Types.ObjectId.isValid(_id)) {
-        logger.error(`No user with id ${_id} exist.`)
-        return res.status(404).send(`No user with id ${_id} exist.`);
-    }
+    logger.info(`updateProfileAvatar starts`);
 
     try {
-        logger.info(`updateUser payload!: ${req.body}`);
-        const updatedUser = await UserModel.findByIdAndUpdate(_id, { ...user, _id }, { new: true });
-        logger.info(`updateUser successful! ${updatedUser._id}`);
-        res.status(202).json(updatedUser);
-    } catch (error) {
-        logger.error(error.message)
-        res.status(408).json({ message: error.message });
-    }
-    logger.info('updateUser ends')
-}
 
-export const deleteUser = async (req, res) => {
-    const { id: _id } = req.params;
+        if (req.method === "DELETE") {
 
-    logger.info(`${API_REQUEST} ${URL_USER}/${_id}`);
-    logger.info(`ndeleteUser starts`);
-    if (!mongoose.Types.ObjectId.isValid(_id)) {
-        logger.error(`No user with id ${_id} exist.`)
-        return res.status(404).send(`No user with id ${_id} exist.`);
-    }
-    try {
-        logger.info(`deleteUser requested: ${_id}`);
-        await UserModel.findByIdAndRemove(_id);
-        logger.info(`deleteUser successful! ${_id}`);
-        res.status(204).json("User Deleted Successfully.");
-    } catch (error) {
-        logger.error(error.message);
-        res.status(408).json({ message: error.message });
-    }
-    logger.info('deleteUser ends');
-}
+            if (!req.user.profileAvatar)
+                throw new ApiError(400, "avatar not found");
 
-export const getFriends = async (req, res) => {
-    const requesterId = req.params.id;
-    logger.info(`${API_REQUEST} ${URL_USER}/${requesterId}/friends`);
-    logger.info(`getFriends starts`);
-    if (!mongoose.Types.ObjectId.isValid(requesterId)) {
-        logger.error(`No user with id ${requesterId} exist.`)
-        return res.status(404).send(`No user with id ${requesterId} exist.`);
-    }
-    try {
-        let friends = await FriendModel.find({ $or: [{ requesterId: requesterId, status: 'A' }, { accepterId: requesterId, status: 'A' }] });
+            const response = await destroyFromCloudinary(`profile-avatar-${req.user._id}`);
 
-        friends = friends.map(friend => mongoose.Types.ObjectId(
-            friend.requesterId === requesterId ? friend.accepterId : friend.requesterId
-        ));
-
-        friends = await UserModel.find({ '_id': { $in: friends } })
-
-        logger.info(`getFriends successful! ${friends.length}`);
-        res.status(200).json(friends);
-    } catch (error) {
-        logger.error(error.message);
-        res.status(408).json({ message: error.message });
-    }
-    logger.info('getFriends ends');
-}
-
-export const friendRequest = async (req, res) => {
-    const id = req.params.id
-    const { requesterId, accepterId, action } = req.body;
-    logger.info(`${API_REQUEST} ${URL_USER}/${id}/friendrequest`);
-    logger.info(`sendFriendRequest starts`);
-    if (!mongoose.Types.ObjectId.isValid(requesterId)) {
-        logger.error(`No user with id ${requesterId} exist.`)
-        return res.status(404).send(`No user with id ${rid} exist.`);
-    }
-    if (!mongoose.Types.ObjectId.isValid(accepterId)) {
-        logger.error(`No user with id ${accepterId} exist.`)
-        return res.status(404).send(`No user with id ${fid} exist.`);
-    }
-    try {
-        if (action === ACT_ADD_FRIEND) {
-            const newRequest = new UserModel(req.body);
-            await newRequest.save();
-            logger.info(`send friendrequest successful!`);
-            res.status(200).json({ message: 'Friend Request is sent successfully.' });
+            if (response.result === "ok") {
+                await UserModel.findByIdAndUpdate(req.user._id, { profileAvatar: "" }, { new: true });
+                res.status(200).json(new ApiResponse(200, response, "avatar deleted successfully"));
+            }
+            else
+                throw new ApiError(500, "error in deleting avatar");
         }
-        else if (action === ACT_ACCEPT_REQUEST) {
-            await FriendModel.findOneAndUpdate(
-                { requesterId: requesterId, accepterId: accepterId },
-                { status: 'A' }
-            );
-            logger.info(`accept friendrequest successful!`);
-            res.status(200).json({ message: 'Friend Request is accepted successfully.' });
+        else {
+
+            const avatarLocalPath = await setUniqueProfileAvatarFileName(req);
+
+            if (!avatarLocalPath)
+                throw new ApiError(400, "avatar file is required");
+            
+            const avatar = await uploadOnCloudinary(avatarLocalPath);
+
+            if (!avatar)
+                throw new ApiError(500, "error in uploading avatar file");
+
+            await UserModel.findByIdAndUpdate(req.user._id, { profileAvatar: avatar.secure_url }, { new: true });
+
+            res.status(200).json(new ApiResponse(200, { _id: req.user._id, profileAvatar: avatar.secure_url }, "avatar uploaded successfully"));
+
         }
 
-
     } catch (error) {
-        logger.error(error.message);
-        res.status(408).json({ message: error.message });
+
+        logger.info(error.stack)
+        error = new ApiError(error?.statusCode || 400, error.message);
+        res.status(error.statusCode).json(error);
     }
-    logger.info('friendRequest ends');
+    logger.info('updateProfileAvatar ends')
 }
+
+// export const createUser = async (req, res) => {
+//     logger.info(`${API_REQUEST} ${URL_USER}`);
+//     logger.info(`createUser starts`);
+//     const newUser = new UserModel(req.body);
+//     try {
+//         logger.info(`createUser payload!: ${req.body}`);
+//         await newUser.save();
+//         logger.info(`createUser successful! ${newUser._id}`);
+//         res.status(201).json(newUser);
+//     } catch (error) {
+//         logger.error(error.message)
+//         res.status(409).json({ message: error.message });
+//     }
+//     logger.info('createUser ends')
+// }
+
+// export const updateUser = async (req, res) => {
+//     const { id: _id } = req.params;
+//     const user = req.body;
+//     logger.info(`${API_REQUEST} ${URL_USER}/${_id}`);
+//     logger.info(`updateUser starts`);
+//     if (!mongoose.Types.ObjectId.isValid(_id)) {
+//         logger.error(`No user with id ${_id} exist.`)
+//         return res.status(404).send(`No user with id ${_id} exist.`);
+//     }
+
+//     try {
+//         logger.info(`updateUser payload!: ${req.body}`);
+//         const updatedUser = await UserModel.findByIdAndUpdate(_id, { ...user, _id }, { new: true });
+//         logger.info(`updateUser successful! ${updatedUser._id}`);
+//         res.status(202).json(updatedUser);
+//     } catch (error) {
+//         logger.error(error.message)
+//         res.status(408).json({ message: error.message });
+//     }
+//     logger.info('updateUser ends')
+// }
+
+// export const deleteUser = async (req, res) => {
+//     const { id: _id } = req.params;
+
+//     logger.info(`${API_REQUEST} ${URL_USER}/${_id}`);
+//     logger.info(`ndeleteUser starts`);
+//     if (!mongoose.Types.ObjectId.isValid(_id)) {
+//         logger.error(`No user with id ${_id} exist.`)
+//         return res.status(404).send(`No user with id ${_id} exist.`);
+//     }
+//     try {
+//         logger.info(`deleteUser requested: ${_id}`);
+//         await UserModel.findByIdAndRemove(_id);
+//         logger.info(`deleteUser successful! ${_id}`);
+//         res.status(204).json("User Deleted Successfully.");
+//     } catch (error) {
+//         logger.error(error.message);
+//         res.status(408).json({ message: error.message });
+//     }
+//     logger.info('deleteUser ends');
+// }
+
+// export const getFriends = async (req, res) => {
+//     const requesterId = req.params.id;
+//     logger.info(`${API_REQUEST} ${URL_USER}/${requesterId}/friends`);
+//     logger.info(`getFriends starts`);
+//     if (!mongoose.Types.ObjectId.isValid(requesterId)) {
+//         logger.error(`No user with id ${requesterId} exist.`)
+//         return res.status(404).send(`No user with id ${requesterId} exist.`);
+//     }
+//     try {
+//         let friends = await FriendModel.find({ $or: [{ requesterId: requesterId, status: 'A' }, { accepterId: requesterId, status: 'A' }] });
+
+//         friends = friends.map(friend => mongoose.Types.ObjectId(
+//             friend.requesterId === requesterId ? friend.accepterId : friend.requesterId
+//         ));
+
+//         friends = await UserModel.find({ '_id': { $in: friends } })
+
+//         logger.info(`getFriends successful! ${friends.length}`);
+//         res.status(200).json(friends);
+//     } catch (error) {
+//         logger.error(error.message);
+//         res.status(408).json({ message: error.message });
+//     }
+//     logger.info('getFriends ends');
+// }
+
+// export const friendRequest = async (req, res) => {
+//     const id = req.params.id
+//     const { requesterId, accepterId, action } = req.body;
+//     logger.info(`${API_REQUEST} ${URL_USER}/${id}/friendrequest`);
+//     logger.info(`sendFriendRequest starts`);
+//     if (!mongoose.Types.ObjectId.isValid(requesterId)) {
+//         logger.error(`No user with id ${requesterId} exist.`)
+//         return res.status(404).send(`No user with id ${rid} exist.`);
+//     }
+//     if (!mongoose.Types.ObjectId.isValid(accepterId)) {
+//         logger.error(`No user with id ${accepterId} exist.`)
+//         return res.status(404).send(`No user with id ${fid} exist.`);
+//     }
+//     try {
+//         if (action === ACT_ADD_FRIEND) {
+//             const newRequest = new UserModel(req.body);
+//             await newRequest.save();
+//             logger.info(`send friendrequest successful!`);
+//             res.status(200).json({ message: 'Friend Request is sent successfully.' });
+//         }
+//         else if (action === ACT_ACCEPT_REQUEST) {
+//             await FriendModel.findOneAndUpdate(
+//                 { requesterId: requesterId, accepterId: accepterId },
+//                 { status: 'A' }
+//             );
+//             logger.info(`accept friendrequest successful!`);
+//             res.status(200).json({ message: 'Friend Request is accepted successfully.' });
+//         }
+
+
+//     } catch (error) {
+//         logger.error(error.message);
+//         res.status(408).json({ message: error.message });
+//     }
+//     logger.info('friendRequest ends');
+// }
 
 
 
