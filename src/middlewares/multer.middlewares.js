5fa0b9c3d5251f2f9137404ed7aaead061b2e12b
Susanta Mandal <susantamandal17@gmail.com>
2024-02-29 01:53:43 +0530

    fileExtension added

--- src/middlewares/multer.middlewares.js
+++ src/middlewares/multer.middlewares.js
@@ -1,13 +1,14 @@
 import multer from "multer";
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, './public/temp')
+    destination: function (_, __, cb) {
+        cb(null, './public/temp');
     },
-    filename: function (req, file, cb) {
-        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-        cb(null, file.fieldname + '-' + uniqueSuffix)
+    filename: function (_, file, cb) {
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+        const fileExtension = file.originalname?.split('.').pop();
+        cb(null, `${file.fieldname}-${uniqueSuffix}${ fileExtension !== file.originalname ? ("."+fileExtension) : ""}`);
     }
-})
+});
 
-export const upload = multer({ storage })
+export const upload = multer({ storage });
