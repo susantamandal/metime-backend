7100620b4f82262d6cbb75640fa35bc16b81a6b6
Susanta Mandal <susantamandal17@gmail.com>
2024-02-29 01:46:41 +0530

    options passed to cloudinary upload method

--- src/utils/cloudinary.utils.js
+++ src/utils/cloudinary.utils.js
@@ -1,24 +1,44 @@
-import { v2 as cloudinary } from "cloudinary"
-import fs from "fs"
-import logger from "../logger";
-        
-cloudinary.config({ 
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET
+import dotenv from "dotenv";
+dotenv.config();
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+import logger from "../logger/index.js";
+import { ApiError } from "./apiError.utils.js";
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
 });
+const options = {
+    use_filename: true,
+    unique_filename: false,
+    overwrite: true,
+    resource_type: "auto"
+};
 
 export const uploadOnCloudinary = async (localFilePath) => {
-    try{
-        if(!localFilePath) return null;
-        const response = await cloudinary.uploader.upload(localFilePath, {
-            resource_type: "auto"
-        });
+    try {
+
+        if (!localFilePath) return null;
+        const response = await cloudinary.uploader.upload(localFilePath, options);
         logger.info(`file has been uploaded on cloudinary ${response.url}`);
+        fs.unlinkSync(localFilePath);
         return response;
 
-    }catch(error){
+    } catch (error) {
+        logger.info(error.stack);
         fs.unlinkSync(localFilePath);
         return null;
     }
-}
+};
+
+export const destroyFromCloudinary = async (publicId) => {
+    try {
+        return await cloudinary.uploader.destroy(publicId, {resource_type: "image"});
+    } catch (error) {
+        logger.info(error.stack);
+        throw new ApiError(500, error.message);
+    }
+};
