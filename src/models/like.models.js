8e7999cf1be92c61f4274550b33f4113b379cc48
Susanta Mandal <susantamandal17@gmail.com>
2024-03-02 23:19:00 +0530

    like model added

---
+++ src/models/like.models.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const likeSchema = mongoose.Schema({
+    like: {
+        type: Boolean,
+        default: true,
+        required: [true, "value is required"],
+    },
+    type:{
+        type: String,
+        enum: {
+            values: ["Post", "Comment"],
+            message: 'value is not supported'
+        },
+        required: [true, "value is required"],
+    },
+    postedTo: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "metaserver_post",
+        immutable: true,
+        required: [true, "value is required"]
+    },
+    createdBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "metaserver_user",
+        immutable: true,
+        required: [true, "value is required"]
+    },
+}, { timestamps: true });
+
+const LikeModel = mongoose.model('metaserver_like', likeSchema);
+
+export default LikeModel;
