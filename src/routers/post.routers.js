0d759f560398d539cc49ffa9185cc7354ffab1da
Susanta Mandal <susantamandal17@gmail.com>
2024-03-02 23:18:17 +0530

    like apis added

--- src/routers/post.routers.js
+++ src/routers/post.routers.js
@@ -1,7 +1,7 @@
 import express from 'express';
 
 import { verifyAccessToken } from '../middlewares/apigateway.middlewares.js';
-import { getPosts, createPost, updatePost, deletePost, getComments, createComment, updateComment, deleteComment} from '../controllers/post.controllers.js'
+import { getPosts, createPost, updatePost, deletePost, getComments, createComment, updateComment, deleteComment, getLikes, createLike} from '../controllers/post.controllers.js'
 import { upload } from "../middlewares/multer.middlewares.js"
 
 const router = express.Router();
@@ -9,11 +9,19 @@ const router = express.Router();
 router.get('/:uid/posts', verifyAccessToken, getPosts);
 router.post('/post',  verifyAccessToken, upload.array('media', 10), createPost );
 router.patch('/post', verifyAccessToken, upload.array('media', 10), updatePost );
-router.delete('/post', verifyAccessToken, deletePost );
+router.delete('/post/:pid', verifyAccessToken, deletePost );
 
 router.get('/:pid/comments', verifyAccessToken, getComments);
 router.post('/comment',  verifyAccessToken, upload.single('media'), createComment );
 router.patch('/comment',  verifyAccessToken, upload.single('media'), updateComment );
-router.delete('/comment', verifyAccessToken, deleteComment );
+router.delete('/comment/:cid', verifyAccessToken, deleteComment );
+
+router.get('/:id/likes', verifyAccessToken, getLikes);
+router.post('/like',  verifyAccessToken, createLike );
+// router.delete('/unlike/:lid',  verifyAccessToken, deleteLike );
+
+// router.get('/:pid/disLikes', verifyAccessToken, getLikes);
+// router.post('/disLike',  verifyAccessToken, createLike );
+// router.patch('/unDisLike/:lid',  verifyAccessToken, deleteLike );
 
 export default router;
