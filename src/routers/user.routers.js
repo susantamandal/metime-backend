9c172fdc0021677b2cea90ded42b2d7eb93d0e98
Susanta Mandal <susantamandal17@gmail.com>
2024-03-02 23:17:53 +0530

    frined reuquest apis added

--- src/routers/user.routers.js
+++ src/routers/user.routers.js
@@ -1,19 +1,21 @@
 import express from "express";
 
-import { getLoginUser, getUser, updateProfileAvatar } from "../controllers/user.controllers.js";
+import { getLoginUserAggregate, getUser, updateProfileAvatar, getUserRequests, getFriendRequests, sendFriendRequest, acceptFriendRequest, deleteFriendRequest, getFriends } from "../controllers/user.controllers.js";
 import { verifyAccessToken } from '../middlewares/apigateway.middlewares.js';
 import { upload } from "../middlewares/multer.middlewares.js"
 
 const router = express.Router();
-// router.post('/', createUser);
-router.get('/loginuser', verifyAccessToken, getLoginUser);
-router.get('/user', verifyAccessToken, getUser);
+
+router.get('/loginuser', verifyAccessToken, getLoginUserAggregate);
+router.get('/user/:id', verifyAccessToken, getUser);
 router.patch('/avatar', verifyAccessToken, upload.single('avatar'), updateProfileAvatar);
 router.delete('/avatar', verifyAccessToken, updateProfileAvatar);
-// router.get('/:id/friends', getFriends);
-// router.get('/:uid/friend/:fid/actions', getActionsOnFriend);
-// router.post('/:id/friendrequest', friendRequest);
-// router.patch('/:id', updateUser);
-// router.delete('/:id', deleteUser);
+
+router.get('/userrequests', verifyAccessToken, getUserRequests);
+router.get('/friendrequests', verifyAccessToken, getFriendRequests);
+router.post('/sendfriendrequest', verifyAccessToken, sendFriendRequest);
+router.patch('/acceptfriendrequest/:rid', verifyAccessToken, acceptFriendRequest);
+router.delete('/deletefriendrequest/:rid', verifyAccessToken, deleteFriendRequest);
+router.get('/friends', verifyAccessToken, getFriends);
 
 export default router;
