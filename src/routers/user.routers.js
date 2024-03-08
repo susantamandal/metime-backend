5e17376099b1c59cf8c256fb7e2251e6a074b261
Susanta Mandal <susantamandal17@gmail.com>
2024-03-09 01:58:43 +0530

    coverphoto and action on user api added

--- src/routers/user.routers.js
+++ src/routers/user.routers.js
@@ -1,6 +1,6 @@
 import express from "express";
 
-import { getLoginUserAggregate, getUser, updateProfileAvatar, getUserRequests, getFriendRequests, sendFriendRequest, acceptFriendRequest, deleteFriendRequest, getFriends } from "../controllers/user.controllers.js";
+import { getLoginUserAggregate, getUser, updateProfileAvatar, updateCoverPhoto, getUserRequests, getFriendRequests, sendFriendRequest, acceptFriendRequest, deleteFriendRequest, getFriends, getActionsOnUser } from "../controllers/user.controllers.js";
 import { verifyAccessToken } from '../middlewares/apigateway.middlewares.js';
 import { upload } from "../middlewares/multer.middlewares.js"
 
@@ -10,12 +10,17 @@ router.get('/loginuser', verifyAccessToken, getLoginUserAggregate);
 router.get('/user/:id', verifyAccessToken, getUser);
 router.patch('/avatar', verifyAccessToken, upload.single('avatar'), updateProfileAvatar);
 router.delete('/avatar', verifyAccessToken, updateProfileAvatar);
+router.patch('/coverphoto', verifyAccessToken, upload.single('coverPhoto'), updateCoverPhoto);
+router.delete('/coverphoto', verifyAccessToken, updateCoverPhoto);
 
 router.get('/userrequests', verifyAccessToken, getUserRequests);
 router.get('/friendrequests', verifyAccessToken, getFriendRequests);
 router.post('/sendfriendrequest', verifyAccessToken, sendFriendRequest);
 router.patch('/acceptfriendrequest/:rid', verifyAccessToken, acceptFriendRequest);
 router.delete('/deletefriendrequest/:rid', verifyAccessToken, deleteFriendRequest);
-router.get('/friends', verifyAccessToken, getFriends);
+router.get('/:uid/friends', verifyAccessToken, getFriends);
+
+router.get('/:uid/actions', verifyAccessToken, getActionsOnUser);
+
 
 export default router;
